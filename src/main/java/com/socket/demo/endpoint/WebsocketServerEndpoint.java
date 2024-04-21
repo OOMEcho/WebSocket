0d09239507f48d37caa2fc7627c7b5e91b9ea2fc
xuesong.lei <228389787@qq.com>
2024-04-21 17:49:32 +0800

    feat: 完善

--- src/main/java/com/socket/demo/endpoint/WebsocketServerEndpoint.java
+++ src/main/java/com/socket/demo/endpoint/WebsocketServerEndpoint.java
@@ -1,9 +1,14 @@
 package com.socket.demo.endpoint;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.socket.demo.handler.MessageHandler;
+import com.socket.demo.message.Message;
 import com.socket.demo.message.impl.AuthRequest;
+import com.socket.demo.util.WebSocketUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.aop.framework.AopProxyUtils;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
@@ -12,9 +17,12 @@ import org.springframework.util.CollectionUtils;
 import javax.annotation.Resource;
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @Author: xuesong.lei
@@ -66,11 +74,30 @@ public class WebsocketServerEndpoint implements InitializingBean {
     @OnMessage
     public void onMessage(Session session, String message) {
         logger.info("[onOpen][session({}) 接收到一条消息({})]", session, message); // 生产环境下，请设置成 debug 级别
+        try {
+            // <1> 获得消息类型
+            JSONObject jsonMessage = JSON.parseObject(message);
+            String messageType = jsonMessage.getString("type");
+            // <2> 获得消息处理器
+            MessageHandler messageHandler = HANDLERS.get(messageType);
+            if (messageHandler == null) {
+                logger.error("[onMessage][消息类型({}) 不存在消息处理器]", messageType);
+                return;
+            }
+            // <3> 解析消息
+            Class<? extends Message> messageClass = this.getMessageClass(messageHandler);
+            // <4> 处理消息
+            Message messageObj = JSON.parseObject(jsonMessage.getString("body"), messageClass);
+            messageHandler.execute(session, messageObj);
+        } catch (Throwable throwable) {
+            logger.info("[onMessage][session({}) message({}) 发生异常]", session, throwable);
+        }
     }
 
     @OnClose
     public void onClose(Session session, CloseReason closeReason) {
         logger.info("[onClose][session({}) 连接关闭。关闭原因是({})}]", session, closeReason);
+        WebSocketUtil.removeSession(session);
     }
 
     @OnError
@@ -78,4 +105,35 @@ public class WebsocketServerEndpoint implements InitializingBean {
         logger.info("[onClose][session({}) 发生异常]", session, throwable);
     }
 
+    private Class<? extends Message> getMessageClass(MessageHandler handler) {
+        // 获得 Bean 对应的 Class 类名。因为有可能被 AOP 代理过。
+        Class<?> targetClass = AopProxyUtils.ultimateTargetClass(handler);
+        // 获得接口的 Type 数组
+        Type[] interfaces = targetClass.getGenericInterfaces();
+        Class<?> superclass = targetClass.getSuperclass();
+        while ((Objects.isNull(interfaces) || 0 == interfaces.length) && Objects.nonNull(superclass)) { // 此处，是以父类的接口为准
+            interfaces = superclass.getGenericInterfaces();
+            superclass = targetClass.getSuperclass();
+        }
+        if (Objects.nonNull(interfaces)) {
+            // 遍历 interfaces 数组
+            for (Type type : interfaces) {
+                // 要求 type 是泛型参数
+                if (type instanceof ParameterizedType) {
+                    ParameterizedType parameterizedType = (ParameterizedType) type;
+                    // 要求是 MessageHandler 接口
+                    if (Objects.equals(parameterizedType.getRawType(), MessageHandler.class)) {
+                        Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
+                        // 取首个元素
+                        if (Objects.nonNull(actualTypeArguments) && actualTypeArguments.length > 0) {
+                            return (Class<Message>) actualTypeArguments[0];
+                        } else {
+                            throw new IllegalStateException(String.format("类型(%s) 获得不到消息类型", handler));
+                        }
+                    }
+                }
+            }
+        }
+        throw new IllegalStateException(String.format("类型(%s) 获得不到消息类型", handler));
+    }
 }
